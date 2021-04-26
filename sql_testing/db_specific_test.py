ab278eeee03500affd909fcd73a969fd134e4c44
Philippsons <philipp.albert1504@gmail.com>
2021-04-26 17:06:12 +0200

    Add file with class for db dialect specific tests

---
+++ sql_testing/db_specific_test.py
@@ -0,0 +1,11 @@
+from .base_test import BaseTest
+
+
+class DbSpecificTest(BaseTest):
+    def __init__(self, engine, path_test_setup, path_to_call, target):
+
+        pass
+
+    def run(self):
+
+        pass
