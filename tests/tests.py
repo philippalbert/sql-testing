9c3128a2ef1599a9d7431e01741e6dcac13a35f4
Philippsons <philipp.albert1504@gmail.com>
2021-04-26 16:22:38 +0200

    Add test for run method of BaseTest

--- tests/tests.py
+++ tests/tests.py
@@ -1,12 +1,11 @@
 import os
-from unittest import TestCase
 from tempfile import TemporaryDirectory
+from unittest import TestCase
 
-from ..sql_test import BaseTest
+from sql_testing.base_test import BaseTest
 
 
 class TestBaseTest(TestCase):
-
     def test_check_file_existence(self):
         """ensure file existence check works as expected
 
@@ -20,14 +19,14 @@ class TestBaseTest(TestCase):
 
             # no error expected
             with open(file_1, "w+") as dummy_file1, open(file_2, "w+") as dummy_file2:
-                dummy_file1.write('something')
-                dummy_file2.write('something')
-                BaseTest(file_1, file_2, 'dummy_target')
+                dummy_file1.write("something")
+                dummy_file2.write("something")
+                BaseTest(file_1, file_2, "dummy_target")
 
             # error expected
             file_3 = os.path.join(temp_dir, "dummy_file3.txt")
             with self.assertRaises(FileNotFoundError):
-                BaseTest(file_1, file_3, 'dummy_target')
+                BaseTest(file_1, file_3, "dummy_target")
 
     def test_read_sql_file(self):
         """test that file reading works as expected"""
@@ -36,16 +35,38 @@ class TestBaseTest(TestCase):
             file_1 = os.path.join(temp_dir, "dummy_file1.txt")
             file_2 = os.path.join(temp_dir, "dummy_file2.txt")
 
-            query_string = 'select * from some_table; select * from another_table'
-            expected = query_string.split(';')
+            query_string = "select * from some_table; select * from another_table"
+            expected = query_string.split(";")
 
             # no error expected
             with open(file_1, "w+") as dummy_file1, open(file_2, "w+") as dummy_file2:
                 dummy_file1.write(query_string)
-                dummy_file2.write('something')
+                dummy_file2.write("something")
 
-                base_test = BaseTest(file_1, file_2, 'dummy_target')
+                base_test = BaseTest(file_1, file_2, "dummy_target")
 
             output = base_test.read_sql_file(file_1)
 
             self.assertEqual(output, expected)
+
+    def test_run(self):
+        """test run method"""
+        expected = [(53.5, "USA"), (47.5, "Germany")]
+        base_test = BaseTest(
+            path_test_setup="fixtures/run_base_test_setup.sql",
+            path_to_call="fixtures/run_base_test_call.sql",
+            target="MEAN_AGE_PER_COUNTRY",
+        )
+
+        with base_test.run() as result:
+            base_test.compare_table_values(result, expected)
+
+        base_test = BaseTest(
+            path_test_setup="fixtures/run_base_test_setup_invalid.sql",
+            path_to_call="fixtures/run_base_test_call.sql",
+            target="MEAN_AGE_PER_COUNTRY",
+        )
+
+        # with self.assertRaises():
+        with base_test.run() as result:
+            base_test.compare_table_values(result, expected)
